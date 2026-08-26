3972e82977d1594b3e14a5c7bff906e5c07f1b26
Aviz <avizway@gmail.com>
2026-08-26 09:18:13 +0530

    tf code added

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket       = "aviz.statelocation"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
+}
+
+resource "aws_instance" "mumbaiserver" {
+  ami                    = "ami-035827357e3c7e810"
+  instance_type          = "t3.micro"
+  key_name               = "awar11-na"
+
+  tags = {
+    Name = "Web-server"
+  }
+}
